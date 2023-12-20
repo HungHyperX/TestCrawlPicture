e45cb3bfda036d0867f1adaa5bc0cf354d0278e0
HungHyperX <131465286+HungHyperX@users.noreply.github.com>
2023-12-20 22:35:05 +0700

    chinh sua lan hai

--- testCrawl.py
+++ testCrawl.py
@@ -1,22 +1,27 @@
 import os
-import requests
-from bs4 import BeautifulSoup
+import requests # pip install requests
+from bs4 import BeautifulSoup # pip install beautifulsoup4
 
-
+# Chọn URL trang cần tải ảnh về
 pageTarget = 'https://this-person-does-not-exist.com/'
 page = requests.get(pageTarget)
+
+# Phân tích HTML bằng Beautiful Soup 4
 soup = BeautifulSoup(page.content, 'html.parser')
+
+# Tìm thẻ <body>
 wrapper = soup.find('body')
 
+# Tìm tất cả có thẻ <img id: "avatar">
 images = wrapper.find_all("img", {'id': 'avatar'})
 
 for image in images:
   imgData = image['src']
-  print(imgData)
+  print(imgData) # In tên ảnh tải về
   if("data:image" not in imgData):
     if(imgData):
 
-      filename = imgData.split('/')[-1]
+      filename = imgData.split('/')[-1] # Tách đường dẫn src để lấy phần ảnh
 
       picSrc = f"{pageTarget}/{imgData}"
       response = requests.get(picSrc)
